7e9431a666ecc7ebce590546d5e85712db5c7a76
MoonVoid <moonvoid@KaliLinuxPurple>
2024-01-21 16:19:05 +0800

    modified readme, hello.cpp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Test0
+# this is a test repo for git usage learning. owo
 Test0 for repository.
---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string in{};
+	cin >> in;
+	if (in == "hello") {
+		cout << "Hello world!";
+	}
+	cin >> in;
+	if (in == "nihao") {
+		cout << "Nihao!";
+	}
+	return 0;
+}
+
