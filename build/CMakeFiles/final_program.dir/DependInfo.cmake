aaeb255e70fdd777f0b87321e3082748f928ed54
MoonVoid <q1586436934@outlook.com>
2024-09-01 15:06:39 +0800

    test

---
+++ build/CMakeFiles/final_program.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/moonvoid/MyStorage/Dev/CppProjects/Learn/src/main.cpp" "CMakeFiles/final_program.dir/src/main.cpp.o" "gcc" "CMakeFiles/final_program.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/moonvoid/MyStorage/Dev/CppProjects/Learn/build/lib/MyMath/CMakeFiles/MyMath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/include/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ProjCMK_VERSION_MAJOR 0
+#define ProjCMK_VERSION_MINOR 1
+#define ProjCMK_VERSION_PATCH 2
