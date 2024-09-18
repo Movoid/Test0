9f9e233f39612c56ecd1afd426af7a804d7dbe81
MoonVoidDev <q1586436934@outlook.com>
2024-09-18 19:03:42 +0800

    CRTP

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,41 @@
 #include <bits/stdc++.h>
 #include "config.h"
 #include "mymath.h"
-
 using namespace std;
+
+template<typename Deri>
+class Base {
+public:
+    template<typename... Args>
+    void interface(Args&&... args) {
+        static_cast<Deri*>(this)->impl(std::forward<Args>(args)...);
+    }
+
+};
+
+
+class A : public Base<A> {
+public:
+    void impl(int a, int b) {
+        cout << "class A impl" << endl;
+        cout << a + b << endl;
+    }
+};
+
+class B : public Base<B> {
+public:
+    void impl(int a, int b) {
+        cout << "class B impl" << endl;
+        cout << a - b << endl;
+    }
+};
+
 
 int main() {
 
-    int a{}, b{};
-    cin >> a >> b;
-    cout << getsum(a, b) << endl;
-    cout << "nihao" << endl;
+    A obj1{};
+    B obj2{};
+    obj1.interface(1, 2);
+    obj2.interface(3, 4);
 
 }
