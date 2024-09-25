cfe634a6ee8fe26985680bc1b2ed8809f59d78ef
MoonVoidDev <q1586436934@outlook.com>
2024-09-25 23:32:21 +0800

    Parser is ok. Function parser is todo.

--- lib/RPNExprParser.h
+++ lib/RPNExprParser.h
@@ -11,20 +11,26 @@ namespace ExprParser {
     class CustomFunctionWrapper {
     private:
         std::function<T(const std::vector<T>&)> func{};
-        size_t argCnt{};
+        std::size_t argCnt{};
         bool limitedArgc{};
+        bool invalid{};
     public:
-        CustomFunctionWrapper(const std::function<T(const std::vector<T>&)> _func, size_t _argCnt = 0, bool _limitedArgc = false) :
+        CustomFunctionWrapper() :
+            invalid{ true } {
+        }
+        CustomFunctionWrapper(const std::function<T(const std::vector<T>&)>& _func, size_t _argCnt = 0, bool _limitedArgc = false) :
             func{ _func },
             argCnt{ _argCnt },
             limitedArgc{ _limitedArgc } {
             if (argCnt && !limitedArgc) throw std::runtime_error{ "[FunctionCall] 函数参数限制初始化错误." };
+            // std::cout << "ctor" << std::endl;
         }
         CustomFunctionWrapper(const CustomFunctionWrapper& obj) = default;
         CustomFunctionWrapper(const CustomFunctionWrapper&& obj) :
             func{ std::move(obj.func) },
             argCnt{ obj.argCnt },
             limitedArgc{ obj.limitedArgc } {
+            // std::cout << "move" << std::endl;
         }
         ~CustomFunctionWrapper() = default;
 
@@ -32,6 +38,8 @@ namespace ExprParser {
             if (limitedArgc && args.size() != argCnt) throw std::runtime_error{ "[FunctionCall] 无效的函数调用, 参数不匹配." };
             return func(args);
         }
+
+        inline bool isInvalid() { return invalid; }
     };
 
     template<typename T, typename Requires = typename std::enable_if_t<!std::is_same_v<T, void>>>
@@ -39,7 +47,11 @@ namespace ExprParser {
     private:
         std::function<T(const T&, const T&)> func{};
         int priority{};
+        bool invalid{};
     public:
+        CustomOperatorWrapper() :
+            invalid{ true } {
+        }
         CustomOperatorWrapper(const std::function<T(const T&, const T&)>& _func, int _priority) :
             func{ _func },
             priority{ _priority } {
@@ -49,7 +61,7 @@ namespace ExprParser {
             func{ std::move(obj.func) },
             priority{ obj.priority } {
         }
-        CustomOperatorWrapper() = default;
+        ~CustomOperatorWrapper() = default;
 
         T operator()(const T& a, const T& b) const {
             return func(a, b);
@@ -58,6 +70,8 @@ namespace ExprParser {
         friend bool operator<(const CustomOperatorWrapper& l, const CustomOperatorWrapper& r) {
             return l.priority < r.priority;
         }
+
+        inline bool isInvalid() { return invalid; }
     };
 
     template<typename T>
@@ -81,6 +95,21 @@ namespace ExprParser {
         std::string rpnExpr{};
         T calcRes{};
 
+    protected:
+
+        inline bool isAlpha(char c) {
+            return (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_');
+        }
+        inline bool isDigit(char c) {
+            return (('0' <= c && c <= '9') || c == '.');
+        }
+        inline bool isDelim(char c) {
+            return c == delim;
+        }
+        inline bool isOp(char c) {
+            return (!isAlpha(c) && !isDigit(c) && !isDelim(c) && c != '(' && c != ')');
+        }
+
     public:
         RPNExprParser() {};
         RPNExprParser(
@@ -100,16 +129,16 @@ namespace ExprParser {
         ~RPNExprParser() = default;
 
         void addOp(char name, const std::function<T(const T&, const T&)>& op, int priority) {
-            if (ops.count(name)) throw std::runtime_error{ "[RPNExprParser] 已存在的操作符." };
-            ops.emplace(name, std::move(Operator<T>{op, priority}));
+            if (ops.count(name)) throw std::runtime_error{ "[Controller] 已存在的操作符." };
+            ops.emplace(name, Operator<T>{op, priority});
         }
         void addVar(std::string_view name, const T& var) {
-            if (vars.count(name)) throw std::runtime_error{ "[RPNExprParser] 已存在的变量名." };
+            if (vars.count(name)) throw std::runtime_error{ "[Controller] 已存在的变量名." };
             vars.emplace(name, var);
         }
-        void addFunc(std::string_view name, const Functor<T>& func, size_t argCnt = 0, bool limitedArgc = false) {
-            if (funcs.count(name)) throw std::runtime_error{ "[RPNExprParser] 已存在的函数名." };
-            funcs.emplace(name, std::move(Functor<T>{func, argCnt, limitedArgc}));
+        void addFunc(std::string_view name, const std::function<T(const std::vector<T>&)>& func, size_t argCnt = 0, bool limitedArgc = false) {
+            if (funcs.count(name)) throw std::runtime_error{ "[Controller] 已存在的函数名." };
+            funcs.emplace(name, Functor<T>{ func, argCnt, limitedArgc });
         }
         void removeOp(char name) { ops.erase(name); }
         void removeVar(std::string_view name) { vars.erase(name); }
@@ -121,6 +150,96 @@ namespace ExprParser {
             originExpr = _originExpr;
             rpnExpr.clear();
             calcRes = {};
+        }
+
+        // 只进行字符串处理
+        std::string parse(std::size_t beg, std::size_t end) {
+            std::stack<char> opStack{};
+            std::string rpn{};
+
+            std::size_t l{ beg }, r{ beg };
+            while (l < end) {
+                while (l < end && isDelim(originExpr[l])) ++l;
+                if (l >= end) break;
+                r = l;
+                // main parse
+                if (isDigit(originExpr[l])) {
+                    // 一定是数字
+                    while (r < end && isDigit(originExpr[r])) ++r;
+                    rpn += (originExpr.substr(l, r - l));
+                    rpn += ' ';
+                    l = r;
+                    continue;
+                }
+                // 解析括号
+                if (originExpr[l] == '(') {
+                    ++r;
+                    opStack.emplace('(');
+                    l = r;
+                    continue;
+                }
+                if (originExpr[l] == ')') {
+                    ++r;
+                    while (!opStack.empty() && opStack.top() != '(') {
+                        if (opStack.top() == '(') break;
+                        rpn += opStack.top();
+                        rpn += ' ';
+                        opStack.pop();
+                    }
+                    if (opStack.empty()) throw std::runtime_error{ "[Parser] 括号不匹配." };
+                    opStack.pop();
+                    l = r;
+                    continue;
+                }
+                if (isOp(originExpr[l])) {
+                    // 一定是单个 char 的运算符
+                    ++r;
+                    if (ops.count(originExpr[l])) {
+                        while (!opStack.empty() && !(ops[opStack.top()] < ops[originExpr[l]])) {
+                            rpn += opStack.top();
+                            rpn += ' ';
+                            opStack.pop();
+                        }
+                        opStack.emplace(originExpr[l]);
+                    }
+                    else {
+                        throw std::runtime_error{ "[Parser] 表达式存在无效的操作符." };
+                    }
+                    l = r;
+                    continue;
+                }
+                if (isAlpha(originExpr[l])) {
+                    while (r < end && (isDigit(originExpr[r]) || isAlpha(originExpr[r]))) {
+                        ++r;
+                    }
+                    if (r < end && originExpr[r] == '(') {
+                        // is function
+                        throw std::runtime_error{ "NOT IMPLEMENTED." };
+                    }
+                    else {
+                        // is variant
+                        std::string_view curVarName{ originExpr.substr(l, r - l) };
+                        if (vars.count(curVarName)) {
+                            rpn += curVarName;
+                            rpn += ' ';
+                        }
+                        else {
+                            throw std::runtime_error{ "[Parser] 表达式存在无效的变量." };
+                        }
+                    }
+                    l = r;
+                    continue;
+                }
+
+                throw std::runtime_error{ "[Parser] 不合法的字符." };
+            }
+            while (!opStack.empty()) {
+                if (opStack.top() == '(') throw std::runtime_error{ "[Parser] 括号不匹配." };
+                rpn += opStack.top();
+                rpn += ' ';
+                opStack.pop();
+            }
+            return rpn;
         }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,46 @@
 
 using namespace std;
 
-class A {
-public:
-    int a{};
-    A(int _a) :
-        a{ _a } {
-        if (_a == 1) throw std::runtime_error{ "woc" };
-    }
-
-    A(A&& obj) :
-        a{ obj.a } {
-        cout << "move" << endl;
-    }
-
-};
-
-unordered_map<int, A> mp{};
-
 
 int main() {
 
+    auto add{ [](const vector<double>& args)->double {return args[0] + args[1]; } };
+    auto sub{ [](const vector<double>& args)->double {return args[0] - args[1]; } };
+    auto mul{ [](const vector<double>& args)->double {return args[0] * args[1]; } };
+    auto div{ [](const vector<double>& args)->double {return args[0] / args[1]; } };
 
+    auto addop{ [](double a, double b)->double {return a + b; } };
+    auto subop{ [](double a, double b)->double {return a - b; } };
+    auto mulop{ [](double a, double b)->double {return a * b; } };
+    auto divop{ [](double a, double b)->double {return a / b; } };
+
+    ExprParser::RPNExprParser<double> a{};
+    a.addFunc("add", add);
+    a.addFunc("sub", sub);
+    a.addFunc("mul", mul);
+    a.addFunc("div", div);
+
+    a.addOp('+', addop, 1);
+    a.addOp('-', subop, 1);
+    a.addOp('*', mulop, 5);
+    a.addOp('/', divop, 5);
+
+    string str1{ "nihao" };
+    string_view str2{ str1 };
+    str1 = "zaijian";
+    cout << str2 << endl;
+
+    a.addVar("aaa", 200);
+    std::string input{ "   (1 + 3 )* 2 + aaa" };
+    a.setExpr(input);
     try {
-        mp.emplace(1, std::move(A{ 1 }));
+        cout << a.parse(0, input.size()) << endl;
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
+
+
 
 
 
