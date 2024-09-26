9d8eb4e70bf0d754ea8c288eb6a3af1520c1080f
MoonVoidDev <q1586436934@outlook.com>
2024-09-26 15:29:54 +0800

    SUPER RPNExprParser

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,54 @@
 #include <bits/stdc++.h>
 #include "RPNExprParser.h"
 
-using namespace std;
 
 
 int main() {
 
-    auto add{ [](const vector<double>& args)->double {return args[0] + args[1]; } };
-    auto sub{ [](const vector<double>& args)->double {return args[0] - args[1]; } };
-    auto mul{ [](const vector<double>& args)->double {return args[0] * args[1]; } };
-    auto div{ [](const vector<double>& args)->double {return args[0] / args[1]; } };
+    auto sum{ [](const std::vector<double>& args) {
+        double sum{};
+        for (auto a : args) sum += a;
+        return sum;
+    } };
+
+    auto max{ [](const std::vector<double>& args) {
+        double ret{args[0]};
+        for (auto a : args) ret = std::max(ret, a);
+        return ret;
+    } };
+
+    auto min{ [](const std::vector<double>& args) {
+        double ret{args[0]};
+        for (auto a : args) ret = std::min(ret, a);
+        return ret;
+    } };
 
     auto addop{ [](double a, double b)->double {return a + b; } };
     auto subop{ [](double a, double b)->double {return a - b; } };
     auto mulop{ [](double a, double b)->double {return a * b; } };
     auto divop{ [](double a, double b)->double {return a / b; } };
+    auto powop{ [](double a, double b)->double {return std::pow(a,b); } };
 
     ExprParser::RPNExprParser<double> a{};
-    a.addFunc("add", add);
-    a.addFunc("sub", sub);
-    a.addFunc("mul", mul);
-    a.addFunc("div", div);
 
     a.addOp('+', addop, 1);
     a.addOp('-', subop, 1);
     a.addOp('*', mulop, 5);
     a.addOp('/', divop, 5);
+    a.addOp('^', powop, 10);
 
-    a.addVar("aaa", 200);
-    a.addVar("pi", 3.1415926535);
+    a.addFunc("sum", sum);
+    a.addFunc("max", max);
+    a.addFunc("min", min);
 
-    double pi{ 3.1415926535 };
-
-    std::string input{ "aaa + add(add(1,2), 3)" };
+    std::string input{ "max(sum(1,2,3,4.4) * 5, 60, 70^2)" };
     a.setExpr(input);
     try {
-        cout << a.parseExpr() << endl;
-        cout << setprecision(10) << a.calcExpr() << endl;
+        std::cout << a.parseExpr() << std::endl;
+        std::cout << std::setprecision(10) << a.calcExpr() << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
-
-
-
-
-
 
 }
