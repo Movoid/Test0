ee9b641096a7db9d004ad4d9bb585dad3c942972
Movoid <q1586436934@gmail.com>
2025-06-08 00:02:59 +0800

    1

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM debian:bookworm AS builder
+
+LABEL maintainer="movoid"
+
+RUN apt update && apt install -y build-essential git cmake
+
+RUN mkdir /app
+WORKDIR /app
+COPY . .
+
+RUN cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/app/install -S /app -B /app/build
+RUN cmake --build /app/build
+RUN cmake --install /app/build
+
+
+FROM debian:bookworm-slim
+
+RUN apt-get update && apt-get install -y libstdc++6
+
+WORKDIR /app
+COPY --from=builder /app/install /usr
+
+RUN useradd -m runner
+USER runner
+
+CMD ["/bin/bash"]
--- src/test.cpp
+++ src/test.cpp
@@ -1,60 +1,7 @@
 #include <bits/stdc++.h>
-#include <fcntl.h>
-#include <sys/epoll.h>
-#include <unistd.h>
 using namespace std;
 
-constexpr int MAX_EVENTS = 5;
+int main() {
 
-int main(int argc, char *argv[]) {
-
-  int epfd{}, ready_cnt{}, fd{}, s{}, j{}, num_open_fds{};
-  struct epoll_event ev{};
-  struct epoll_event evlist[MAX_EVENTS]{};
-
-  char buf[BUFSIZ]{};
-
-  epfd = epoll_create1(EPOLL_CLOEXEC);
-
-  for (int j = 1; j < argc; j++) {
-    if ((fd = open(argv[j], O_RDONLY)) == -1) {
-      perror("open");
-    }
-    printf("opened \"%s\" on fd %d\n", argv[j], fd);
-    ev.events = EPOLLIN;
-    ev.data.fd = fd;
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-      perror("epoll_ctl");
-    }
-  }
-
-  num_open_fds = argc - 1;
-
-  while (num_open_fds > 0) {
-    printf("About to epoll_wait\n");
-    if ((ready_cnt = epoll_wait(epfd, evlist, MAX_EVENTS, -1)) == -1) {
-      if (errno == EINTR) {
-        continue;
-      } else {
-        perror("epoll_wait");
-      }
-    }
-    printf("ready cnt: %d.\n", ready_cnt);
-    for (j = 0; j < ready_cnt; j++) {
-      printf("\tfd=%dp; events:%s%s%s\n", evlist[j].data.fd, (evlist[j].events & EPOLLIN) ? "EPOLLIN" : "",
-             (evlist[j].events & EPOLLHUP) ? "EPOLLHUP" : "", (evlist[j].events & EPOLLERR) ? "EPOLLERR" : "");
-      if (evlist[j].events & EPOLLIN) {
-        if ((s = read(evlist[j].data.fd, buf, sizeof(buf))) == -1) {
-          perror("read");
-        }
-      } else if (evlist[j].events & (EPOLLHUP | EPOLLERR)) {
-        printf("\tclosing fd %d.\n", evlist[j].data.fd);
-      }
-      if (close(evlist[j].data.fd) == -1) {
-        perror("close");
-      }
-      num_open_fds--;
-    }
-  }
-  puts("bye");
+  cout << "fin" << endl;
 }
